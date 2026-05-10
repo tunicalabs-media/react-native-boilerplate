95998d7b236199e7edcc3170b03b46fd05767c7c
Mridul Panda <immridulpanda2820@gmail.com>
2026-05-10 11:24:20 +0530

    Update MainActivity to support RNScreens and modify package dependencies

--- android/app/src/main/java/com/nativeboilerplate/MainActivity.kt
+++ android/app/src/main/java/com/nativeboilerplate/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.nativeboilerplate
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.swmansion.rnscreens.fragment.restoration.RNScreensFragmentFactory
 
 class MainActivity : ReactActivity() {
 
@@ -12,6 +14,11 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "NativeBoilerplate"
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    supportFragmentManager.fragmentFactory = RNScreensFragmentFactory()
+    super.onCreate(savedInstanceState)
+  }
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
