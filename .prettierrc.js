3c51a90c1f2b72a159c8507a093e0a4b139f591c
Mridul Panda <immridulpanda2820@gmail.com>
2026-05-10 11:10:34 +0530

    Update ESLint and Prettier configurations, add new scripts, and refine asset management

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,9 @@ module.exports = {
   arrowParens: 'avoid',
   singleQuote: true,
   trailingComma: 'all',
+  useTabs: false,
+  semi: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  printWidth: 80,
 };
